ad62c8c56bbdee693399297eb16b7ae5a1d61722
Gerardo Lagunes García <gerardo.lagunes@ctb.upm.es>
2019-12-11 23:55:24 +0100

    Initial code

--- RhdwikiApplication.java
+++ RhdwikiApplication.java
@@ -2,7 +2,7 @@ package edu.ctb.upm.midas.rhdwiki;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//TEST!
+
 @SpringBootApplication
 public class RhdwikiApplication {
 
